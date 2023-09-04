01017ea0e937335a512451901b4c0ca0ceaacdd0
duaij304 <duaijshadeed@gmail.com>
2023-09-04 06:37:11 +0300

    version 1

---
+++ Myshark.py
@@ -0,0 +1,106 @@
+shark_wifi_ascii = r"""
+(..       \_    ,  |\  /|
+ \       O  \  /|  \ \/ /
+  \______    \/ |   \  / 
+     vvvv\    \ |   /  |
+     \^^^^  ==   \_/   |
+      `\_   ===    \.  |
+      / /\_   \ /      |
+      |/   \_  \|      /
+ snd         \________/
+"""
+
+
+
+import pyshark
+import subprocess
+import shutil
+import pyfiglet
+
+def start_capture(interface):
+    global capture_process
+    capture_process = subprocess.Popen(["tshark", "-i", interface, "-w", "capture.pcap"])
+    print(f"Capture started on interface {interface}! Press 'Ctrl+C' to stop and analyze.")
+
+def stop_capture():
+    capture_process.terminate()
+    print("Capture stopped. Analyzing captured packets...")
+
+def analyze_packets():
+    print("Analyzing captured packets:")
+    capture = pyshark.FileCapture("capture.pcap")
+    packet_count = 0
+
+    devices = {}
+    ports = {}
+
+    for packet in capture:
+        packet_count += 1
+
+        src_ip = packet.ip.src
+        dst_ip = packet.ip.dst
+
+        src_port = packet[packet.transport_layer].srcport
+        dst_port = packet[packet.transport_layer].dstport
+
+        devices[src_ip] = devices.get(src_ip, 0) + 1
+        devices[dst_ip] = devices.get(dst_ip, 0) + 1
+
+        ports[src_port] = ports.get(src_port, 0) + 1
+        ports[dst_port] = ports.get(dst_port, 0) + 1
+
+    print(f"Total packets captured: {packet_count}")
+    print("Device Information:")
+    for device, count in devices.items():
+        print(f"Device: {device}, Packets: {count}")
+
+    print("Port Information:")
+    for port, count in ports.items():
+        print(f"Port: {port}, Packets: {count}")
+
+def save_capture():
+    shutil.copy("capture.pcap", "saved_capture.pcap")
+    print("Capture saved to 'saved_capture.pcap'.")
+
+def load_capture():
+    shutil.copy("saved_capture.pcap", "capture.pcap")
+    print("Capture loaded from 'saved_capture.pcap'.")
+
+codesnail_ascii = pyfiglet.figlet_format("MyShark")
+
+print(shark_wifi_ascii)
+print(codesnail_ascii)
+print("Welcome to the MyShark Wireshark Companion!")
+
+capture_process = None
+
+try:
+    while True:
+        print("\nMenu:")
+        print("1. Start Capture")
+        print("2. Stop Capture")
+        print("3. Analyze Packets")
+        print("4. Save Capture")
+        print("5. Load Capture")
+        print("6. Quit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            interface = input("Enter the capture interface (e.g., eth0): ")
+            start_capture(interface)
+        elif choice == "2":
+            stop_capture()
+        elif choice == "3":
+            analyze_packets()
+        elif choice == "4":
+            save_capture()
+        elif choice == "5":
+            load_capture()
+        elif choice == "6":
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
+
+except KeyboardInterrupt:
+    print("\nKeyboard interrupt detected. Exiting...")

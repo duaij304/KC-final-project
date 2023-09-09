d51b88757cac8b255544ed699a8a9bf372ae2044
duaij304 <duaijshadeed@gmail.com>
2023-09-09 12:54:58 +0300

    1.5 update
    finished Customtkinter and improved the analysis function

--- Myshark_windows.py
+++ Myshark_windows.py
@@ -1,134 +1,186 @@
-shark_wifi_ascii = r"""
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣶⣶⣿⣷⣶⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣦⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⠿⠟⠛⠛⠉⠉⣉⣉⣉⣉⣉⣛⣛⡻⠿⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣨⣿⣿⠟⠋⢁⣀⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿⠋⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⡀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡿⠋⣀⣴⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠏⢀⣴⣿⣿⣿⣿⠏⣼⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣤⣈⣙⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⣻⣿⣿⠆
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠃⢠⣿⣿⣿⣿⣿⡏⠀⢻⣿⣿⠘⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣥⣤⣤⣴⣾⣿⠟⠁⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠾⠃⢀⣿⣿⣿⣿⣿⣿⣧⡀⠈⠻⣿⣦⠈⠻⠄⢹⣿⣿⣿⡿⠟⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣽⣿⠟⠁⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣨⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⠀⠀⠀⢀⣴⠾⠛⠉⠁⠀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⠟⠁⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠉⠀⠀⠀⠀⠀⢸⣿⣦⣶⣰⣧⣠⣦⠀⣴⣆⢀⣦⣀⣴⣄⣴⣿⣿⣿⣿⣶⣶⣤⣄⡀⠀
-⠀⠀⠀⠀⣀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠿⠟⠛⠛⠛⠛⠛⠛⠛⠓
-⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠟⠛⠋⠉⠀⠀⣀⣠⣤⣶⡶⠶⠶⠶⠶⠶⢶⣶⣦⣤⣄⣙⠀⠘⠟⠙⢿⡿⢻⣿⡿⣿⡿⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠻⢿⣷⣶⣤⣀⡀⠀⠉⠀⠈⣁⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠿⠿⠿⠿⠿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⣤⡘⣿⡿⠛⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⠛⢧⠈⠁⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠿⠁⠀⠈⠀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⡀⠀⣈⡙⠻⠿⢷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⠀⠘⠻⢷⣶⣦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡆⠀⠀⢨⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⠀⠀⢸⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡆⠀⢸⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣇⣸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"""
-
-
-
-import pyshark
 import subprocess
-import shutil
-import pyfiglet
-import alert 
-
-def start_capture(interface):
-    global capture_process
-    tshark_path = r"C:\Program Files\Wireshark\tshark.exe"  
-    capture_process = subprocess.Popen([tshark_path, "-i", interface, "-w", "capture.pcap"])
-    print(f"Capture started on interface {interface}! Press 'Ctrl+C' to stop and analyze.")
-
-def stop_capture():
-    capture_process.terminate()
-    print("Capture stopped. Analyzing captured packets...")
-
-def analyze_packets():
-    print("Analyzing captured packets:")
-    capture = pyshark.FileCapture("capture.pcap")
-    packet_count = 0
-
-    devices = {}
-    ports = {}
-
-    for packet in capture:
-        packet_count += 1
-
-        src_ip = packet.ip.src
-        dst_ip = packet.ip.dst
-
-        if hasattr(packet, "transport_layer"):
-            src_port = packet[packet.transport_layer].srcport
-            dst_port = packet[packet.transport_layer].dstport
-
-            devices[src_ip] = devices.get(src_ip, 0) + 1
-            devices[dst_ip] = devices.get(dst_ip, 0) + 1
-
-            ports[src_port] = ports.get(src_port, 0) + 1
-            ports[dst_port] = ports.get(dst_port, 0) + 1
-
-    print(f"Total packets captured: {packet_count}")
-    print("Device Information:")
-    for device, count in devices.items():
-        print(f"Device: {device}, Packets: {count}")
-
-    print("Port Information:")
-    for port, count in ports.items():
-        print(f"Port: {port}, Packets: {count}")
-
-def save_capture():
-    shutil.copy("capture.pcap", "saved_capture.pcap")
-    print("Capture saved to 'saved_capture.pcap'.")
-
-def load_capture():
-    shutil.copy("saved_capture.pcap", "capture.pcap")
-    print("Capture loaded from 'saved_capture.pcap'.")
-
-codesnail_ascii = pyfiglet.figlet_format("MyShark")
-
-print(shark_wifi_ascii)
-print(codesnail_ascii)
-print("Welcome to the MyShark Wireshark Companion!")
+import pyshark
+import threading
+import tkinter as tk
+from tkinter import ttk, messagebox
+from datetime import datetime
+import psutil
+import customtkinter
 
 capture_process = None
+capture_thread = None
+stop_capture_flag = False
+captured_packets = []
 
-try:
-    while True:
-        print("\nMenu:")
-        print("1. Start Capture")
-        print("2. Stop Capture")
-        print("3. Analyze Packets")
-        print("4. Save Capture")
-        print("5. Load Capture")
-        print("6. Quit")
+tshark_path = r"C:\Program Files\Wireshark\tshark.exe"
 
-        choice = input("Enter your choice: ")
+def get_available_interfaces():
+    interfaces = [iface for iface, addrs in psutil.net_if_addrs().items()]
+    return interfaces
 
-        if choice == "1":
-            interface = input("Enter the capture interface (e.g., eth0): ")
-            start_capture(interface)
-        elif choice == "2":
-            stop_capture()
-        elif choice == "3":
-            analyze_packets()
-        elif choice == "4":
-            save_capture()
-        elif choice == "5":
-            load_capture()
-        elif choice == "6":
-            break
-        else:
-            print("Invalid choice. Please enter a valid option.")
+def start_capture(interface, capture_filter=None):
+    global capture_thread
+    global stop_capture_flag
 
-except KeyboardInterrupt:
-    print("\nKeyboard interrupt detected. Exiting...")
+    if capture_thread and capture_thread.is_alive():
+        messagebox.showwarning("Warning", "Capture is already running. Please stop the existing capture first.")
+        return
+
+    stop_capture_flag = False
+    capture_thread = threading.Thread(target=capture_packets, args=(interface, capture_filter))
+    capture_thread.start()
+
+def capture_packets(interface, capture_filter):
+    global capture_process
+
+    if not tshark_path:
+        messagebox.showerror("Error", "Wireshark (tshark) not found in your PATH. Please install Wireshark.")
+        return
+
+    args = [tshark_path, "-i", interface, "-w", "capture.pcap"]
+    if capture_filter:
+        args += ["-f", capture_filter]
+
+    try:
+        capture_process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        messagebox.showinfo("Info", f"Capture started on interface {interface}! Press 'Stop Capture' to stop.")
+        capture_process.communicate() 
+        if not stop_capture_flag:
+            messagebox.showerror("Error", "Capture process terminated unexpectedly. Please check your settings and try again.")
+    except Exception as e:
+        messagebox.showerror("Error", f"Error starting capture: {str(e)}")
+
+def stop_capture():
+    global capture_process
+    global stop_capture_flag
+
+    if capture_process and capture_process.poll() is None:
+        capture_process.terminate()
+        capture_process.wait()
+        messagebox.showinfo("Info", "Capture stopped.")
+        stop_capture_flag = True
+    else:
+        messagebox.showwarning("Warning", "Capture process is not running.")
+
+def analyze_packets():
+    global captured_packets
+    captured_packets = []  
+    packet_count = 0
+    source_devices = {}
+    destination_devices = {}
+    source_ports = {}
+
+    try:
+        capture = pyshark.FileCapture("capture.pcap")
+
+        for packet in capture:
+            packet_count += 1
+
+            src_ip = packet.ip.src if hasattr(packet, "ip") else "Unknown"
+            dst_ip = packet.ip.dst if hasattr(packet, "ip") else "Unknown"
+
+            src_port = None
+            dst_port = None
+
+            if hasattr(packet, "transport_layer"):
+                src_port = packet[packet.transport_layer].srcport
+                dst_port = packet[packet.transport_layer].dstport
+
+            if src_ip != "Unknown":
+                source_devices[src_ip] = source_devices.get(src_ip, 0) + 1
+
+            if dst_ip != "Unknown":
+                destination_devices[dst_ip] = destination_devices.get(dst_ip, 0) + 1
+
+            if src_port:
+                source_ports[src_port] = source_ports.get(src_port, 0) + 1
+
+            if dst_port:
+                source_ports[dst_port] = source_ports.get(dst_port, 0) + 1
+
+    except Exception as e:
+        messagebox.showerror("Error", f"Error analyzing packets: {str(e)}")
+
+    result_text = f"Total packets captured: {packet_count}\n\n"
+    result_text += "Source Devices Information:\n"
+    for device, count in source_devices.items():
+        result_text += f"Source Device: {device}, Packets: {count}\n"
+
+    result_text += "\nDestination Devices Information:\n"
+    for device, count in destination_devices.items():
+        result_text += f"Destination Device: {device}, Packets: {count}\n"
+
+    result_text += "\nSource Ports Information:\n"
+    for port, count in source_ports.items():
+        result_text += f"Source Port: {port}, Packets: {count}\n"
+
+    result_window = tk.Toplevel()
+    result_window.title("Packet Analysis Results")
+
+    result_text_widget = tk.Text(result_window, wrap=tk.WORD)
+    result_text_widget.pack(expand=True, fill='both')
+
+    result_text_widget.insert(tk.END, result_text)
+
+    scrollbar = tk.Scrollbar(result_text_widget, command=result_text_widget.yview)
+    scrollbar.pack(side=tk.RIGHT, fill='y')
+
+    result_text_widget.config(yscrollcommand=scrollbar.set)
+
+    result_text_widget.config(state=tk.DISABLED)
+
+def export_results():
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d_%H%M%S")
+    export_filename = f"packet_analysis_{timestamp}.txt"
+
+    with open(export_filename, "w") as file:
+        sys.stdout = file
+        analyze_packets()
+        sys.stdout = sys.__stdout__
+
+    messagebox.showinfo("Info", f"Results exported to {export_filename}")
+
+def main():
+    root = customtkinter.CTk()
+    root.title("MyShark Wireshark Companion")
+
+    frame = customtkinter.CTkFrame(root)
+    frame.pack()
+
+    def get_selected_interface():
+        return interface_combobox.get()
+
+    interface_label = customtkinter.CTkLabel(frame, text="Select Interface:")
+    interface_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+    available_interfaces = get_available_interfaces()
+    interface_combobox = customtkinter.CTkComboBox(frame, values=available_interfaces, state="readonly")
+    interface_combobox.grid(row=0, column=1, padx=5, pady=5)
+    interface_combobox.set(available_interfaces[0])
+
+    capture_filter_label = customtkinter.CTkLabel(frame, text="Capture Filter (optional):")
+    capture_filter_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+
+    capture_filter_entry = customtkinter.CTkEntry(frame)
+    capture_filter_entry.grid(row=1, column=1, padx=5, pady=5)
+
+    start_capture_button = customtkinter.CTkButton(frame, text="Start Capture", command=lambda: start_capture(get_selected_interface(), capture_filter_entry.get()))
+    start_capture_button.grid(row=2, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    stop_capture_button = customtkinter.CTkButton(frame, text="Stop Capture", command=stop_capture)
+    stop_capture_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    analyze_packets_button = customtkinter.CTkButton(frame, text="Analyze Captured Packets", command=analyze_packets)
+    analyze_packets_button.grid(row=4, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    export_results_button = customtkinter.CTkButton(frame, text="Export Results to File", command=export_results)
+    export_results_button.grid(row=6, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    quit_button = customtkinter.CTkButton(frame, text="Quit", command=root.quit)
+    quit_button.grid(row=8, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
--- mysharkadvanced.py
+++ mysharkadvanced.py
@@ -1,90 +1,195 @@
-import tkinter as tk
-from tkinter import ttk
+import subprocess
 import pyshark
 import threading
-import queue
+import tkinter as tk
+from tkinter import ttk, messagebox
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import psutil
+import customtkinter
 
+capture_process = None
 capture_thread = None
-stop_capture_event = threading.Event()
-packet_queue = queue.Queue()
+stop_capture_flag = False
+captured_packets = []
 
-def start_capture(interface):
-    global capture_thread, stop_capture_event
-    stop_capture_event.clear()
-    capture_thread = threading.Thread(target=capture_packets, args=(interface,))
+tshark_path = r"C:\Program Files\Wireshark\tshark.exe"
+
+
+def get_available_interfaces():
+    interfaces = [iface for iface, addrs in psutil.net_if_addrs().items()]
+    return interfaces
+
+
+def start_capture(interface, capture_filter=None):
+    global capture_thread
+    global stop_capture_flag
+
+    if capture_thread and capture_thread.is_alive():
+        messagebox.showwarning("Warning", "Capture is already running. Please stop the existing capture first.")
+        return
+
+    stop_capture_flag = False
+    capture_thread = threading.Thread(target=capture_packets, args=(interface, capture_filter))
     capture_thread.start()
 
-def stop_capture():
-    global stop_capture_event
-    stop_capture_event.set()
 
-def capture_packets(interface):
-    # Set up asyncio event loop in this thread
-    import asyncio
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    
-    capture = pyshark.LiveCapture(interface=interface)
-    for packet in capture.sniff_continuously():
-        if stop_capture_event.is_set():
-            break
-        packet_queue.put(packet)
+def capture_packets(interface, capture_filter):
+    global capture_process
 
-def update_gui():
-    while not stop_capture_event.is_set():
-        try:
-            packet = packet_queue.get(timeout=0.1)
-            real_time_packet_analysis(packet)
-        except queue.Empty:
-            pass
+    if not tshark_path:
+        messagebox.showerror("Error", "Wireshark (tshark) not found in your PATH. Please install Wireshark.")
+        return
 
-def real_time_packet_analysis(packet):
+    args = [tshark_path, "-i", interface, "-w", "capture.pcap"]
+    if capture_filter:
+        args += ["-f", capture_filter]
+
     try:
-        if hasattr(packet, "ip"):
-            src_ip = packet.ip.src
-            dst_ip = packet.ip.dst
+        capture_process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        messagebox.showinfo("Info", f"Capture started on interface {interface}! Press 'Stop Capture' to stop.")
+        capture_process.communicate()  # Wait for the capture process to finish
+        if not stop_capture_flag:
+            messagebox.showerror("Error", "Capture process terminated unexpectedly. Please check your settings and try again.")
+    except Exception as e:
+        messagebox.showerror("Error", f"Error starting capture: {str(e)}")
 
-            transport_layer = packet.transport_layer if hasattr(packet, "transport_layer") else None
-            src_port = packet[transport_layer].srcport if transport_layer else None
-            dst_port = packet[transport_layer].dstport if transport_layer else None
-            protocol = transport_layer if transport_layer else "Unknown"
 
-            output_text.insert(tk.END, f"Source IP: {src_ip}, Source Port: {src_port}\n")
-            output_text.insert(tk.END, f"Destination IP: {dst_ip}, Destination Port: {dst_port}\n")
-            output_text.insert(tk.END, f"Protocol: {protocol}\n")
-            output_text.update()  # Update the GUI
+def stop_capture():
+    global capture_process
+    global stop_capture_flag
+
+    if capture_process and capture_process.poll() is None:
+        capture_process.terminate()
+        capture_process.wait()
+        messagebox.showinfo("Info", "Capture stopped.")
+        stop_capture_flag = True
+    else:
+        messagebox.showwarning("Warning", "Capture process is not running.")
+
+
+def analyze_packets():
+    global captured_packets
+    captured_packets = []  # Clear previous captured packets
+    packet_count = 0
+    devices = {}
+    ports = {}
+
+    try:
+        capture = pyshark.FileCapture("capture.pcap")
+
+        for packet in capture:
+            packet_count += 1
+
+            src_ip = packet.ip.src if hasattr(packet, "ip") else "Unknown"
+            dst_ip = packet.ip.dst if hasattr(packet, "ip") else "Unknown"
+
+            if hasattr(packet, "transport_layer"):
+                src_port = packet[packet.transport_layer].srcport
+                dst_port = packet[packet.transport_layer].dstport
+
+                devices[src_ip] = devices.get(src_ip, 0) + 1
+                devices[dst_ip] = devices.get(dst_ip, 0) + 1
+
+                ports[src_port] = ports.get(src_port, 0) + 1
+                ports[dst_port] = ports.get(dst_port, 0) + 1
 
     except Exception as e:
-        output_text.insert(tk.END, f"An error occurred while analyzing the packet: {e}\n")
+        messagebox.showerror("Error", f"Error analyzing packets: {str(e)}")
 
-root = tk.Tk()
-root.title("Real-Time Packet Analysis")
+    result_text = f"Total packets captured: {packet_count}\n\n"
+    result_text += "Device Information:\n"
+    for device, count in devices.items():
+        result_text += f"Device: {device}, Packets: {count}\n"
 
-root = tk.Tk()
-root.title("Real-Time Packet Analysis")
+    result_text += "\nPort Information:\n"
+    for port, count in ports.items():
+        result_text += f"Port: {port}, Packets: {count}\n"
 
-label = tk.Label(root, text="Enter the capture interface (e.g., eth0):")
-label.pack()
+    result_window = tk.Toplevel()
+    result_window.title("Packet Analysis Results")
+    result_label = tk.Label(result_window, text=result_text, padx=20, pady=20)
+    result_label.pack()
 
-interface_entry = tk.Entry(root)
-interface_entry.pack()
 
-def start_capture_button():
-    interface = interface_entry.get()
-    start_capture(interface)
-start_button = ttk.Button(root, text="Start Capture", command=start_capture_button)
-start_button.pack()
+def visualize_data():
+    if not captured_packets:
+        messagebox.showinfo("Info", "No captured packets to visualize.")
+        return
 
-stop_button = ttk.Button(root, text="Stop Capture", command=stop_capture)
-stop_button.pack()
+    df = pd.DataFrame(captured_packets)
 
-output_text = tk.Text(root, height=20, width=50)
-output_text.pack()
+    # Visualization example (you can modify this)
+    protocol_counts = df["Protocol"].value_counts()
+    plt.figure(figsize=(8, 6))
+    protocol_counts.plot(kind="bar")
+    plt.title("Protocol Distribution")
+    plt.xlabel("Protocol")
+    plt.ylabel("Count")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
 
-update_thread = threading.Thread(target=update_gui)
-update_thread.daemon = True
-update_thread.start()
 
-root.mainloop()
+def export_results():
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d_%H%M%S")
+    export_filename = f"packet_analysis_{timestamp}.txt"
 
-root.mainloop()
+    with open(export_filename, "w") as file:
+        sys.stdout = file
+        analyze_packets()
+        sys.stdout = sys.__stdout__
+
+    messagebox.showinfo("Info", f"Results exported to {export_filename}")
+
+def main():
+    root = customtkinter.CTk()
+    root.title("MyShark Wireshark Companion")
+
+    frame = customtkinter.CTkFrame(root)
+    frame.pack()
+
+    def get_selected_interface():
+        return interface_combobox.get()
+
+    interface_label = customtkinter.CTkLabel(frame, text="Select Interface:")
+    interface_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+    available_interfaces = get_available_interfaces()
+    interface_combobox = customtkinter.CTkComboBox(frame, values=available_interfaces, state="readonly")
+    interface_combobox.grid(row=0, column=1, padx=5, pady=5)
+    interface_combobox.set(available_interfaces[0])
+
+    capture_filter_label = customtkinter.CTkLabel(frame, text="Capture Filter (optional):")
+    capture_filter_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+
+    capture_filter_entry = customtkinter.CTkEntry(frame)
+    capture_filter_entry.grid(row=1, column=1, padx=5, pady=5)
+
+    start_capture_button = customtkinter.CTkButton(frame, text="Start Capture", command=lambda: start_capture(get_selected_interface(), capture_filter_entry.get()))
+    start_capture_button.grid(row=2, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    stop_capture_button = customtkinter.CTkButton(frame, text="Stop Capture", command=stop_capture)
+    stop_capture_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    analyze_packets_button = customtkinter.CTkButton(frame, text="Analyze Captured Packets", command=analyze_packets)
+    analyze_packets_button.grid(row=4, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    live_packet_stats_button = customtkinter.CTkButton(frame, text="Live Packet Statistics", command=lambda: live_packet_stats(get_selected_interface(), capture_filter_entry.get()))
+    live_packet_stats_button.grid(row=5, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    export_results_button = customtkinter.CTkButton(frame, text="Export Results to File", command=export_results)
+    export_results_button.grid(row=6, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    visualize_button = customtkinter.CTkButton(frame, text="Visualize Data", command=visualize_data)
+    visualize_button.grid(row=7, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    quit_button = customtkinter.CTkButton(frame, text="Quit", command=root.quit)
+    quit_button.grid(row=8, column=0, columnspan=2, padx=5, pady=10, sticky="we")
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
